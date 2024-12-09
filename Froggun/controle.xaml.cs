00d95c86131a2b71b6d8bf6d1d098db1b61b6503
Jules Camelin <Jules.Camelin@etu.univ-smb.fr>
2024-12-09 17:46:07 +0100

    ajout des canva en plus

---
+++ Froggun/controle.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Froggun
+{
+    /// <summary>
+    /// Logique d'interaction pour controle.xaml
+    /// </summary>
+    public partial class controle : Window
+    {
+        public controle()
+        {
+            InitializeComponent();
+        }
+
+        private void BoutonDifficulter(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BoutonSon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BoutonFond_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
