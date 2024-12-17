85dde0c5d720a04b3b19088527c5ff36cc8953f8
Jules Camelin <Jules.Camelin@etu.univ-smb.fr>
2024-12-17 15:12:16 +0100

    fin_aide

--- Froggun/aide.xaml.cs
+++ Froggun/aide.xaml.cs
@@ -22,12 +22,12 @@ namespace Froggun
         public aide()
         {
             InitializeComponent();
-            labelGauche.Content = Touche.ToucheGauche + " pour déplacer la grenouille vers la gauche.";
-            labelDroite.Content = Touche.ToucheDroite + " pour déplacer la grenouille vers la droite.";
-            labelBas.Content = Touche.ToucheBas + " pour déplacer la grenouille vers le bas.";
-            labelHaut.Content =  Touche.ToucheHaut + " pour déplacer la grenouille vers le haut.";
-            labelRoulade.Content = Touche.ToucheRoulade + "pour rouler / esquiver et aller plus vite.";
-            labelPause.Content = Touche.TouchePause + " pour mettre le jeu en pause.";
+            labelGauche.Content = "     - "+Touche.ToucheGauche + " pour déplacer la grenouille vers la gauche.";
+            labelDroite.Content = "     - " + Touche.ToucheDroite + " pour déplacer la grenouille vers la droite.";
+            labelBas.Content = "     - " + Touche.ToucheBas + " pour déplacer la grenouille vers le bas.";
+            labelHaut.Content = "     - " + Touche.ToucheHaut + " pour déplacer la grenouille vers le haut.";
+            labelRoulade.Content = "     - " + Touche.ToucheRoulade + " pour rouler / esquiver et aller plus vite.";
+            labelPause.Content = "     - " + Touche.TouchePause + " pour mettre le jeu en pause.";
         }
 
         
